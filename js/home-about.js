e1b466f865bb8c721b6d65399d146a30768cbc04
Lana Ahmed <lana.e.ahmed@gmail.com>
2025-11-20 19:01:51 +0300

    .

---
+++ js/home-about.js
@@ -0,0 +1,7 @@
+/*to show and hide the mobile menu */
+const mobileNav=document.querySelector(".mobile-nav");
+const mobileMenu =document.querySelector(".mobile-menu");
+
+ mobileNav.addEventListener("click",()=>{
+    mobileMenu.classList.toggle("active");
+ });
